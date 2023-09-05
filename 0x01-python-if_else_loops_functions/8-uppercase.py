2218b39ebeff721f9de54b955495158e0c75794d
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-09-05 22:11:56 +0100

    Task checks 7

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) <= 122 and ord(c) >= 97:
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(
+            chr(ord(c) - 32) if ord(c) <= 122 and ord(c) >= 97 else c), end="")
+    print("".format())
+# uppercase("this is a test")
