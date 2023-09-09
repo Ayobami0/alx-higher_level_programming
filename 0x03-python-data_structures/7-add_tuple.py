86f8ce0dbb599e9309a870e713fe303c079b2520
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-09-09 16:59:07 +0100

    pycodestyle fix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,10 +7,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         + (tuple_b[1] if len(tuple_b) >= 2 else 0)
     )
 # print(add_tuple((1, 3), (5, 5)))
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+# tuple_a = (1, 89)
+# tuple_b = (88, 11)
+# new_tuple = add_tuple(tuple_a, tuple_b)
+# print(new_tuple)
+#
+# print(add_tuple(tuple_a, (1, )))
+# print(add_tuple(tuple_a, ()))
