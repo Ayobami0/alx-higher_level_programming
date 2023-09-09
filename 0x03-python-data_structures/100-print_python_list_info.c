ea2c6a6a3248ca3023f027cfe410ed50ca566454
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-09-09 21:25:30 +0100

    Betty style fix

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,21 +4,28 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * print_python_list_info - Print information about a Python list object.
+ *
+ * This function takes a Python list object 'p' as input
+ * and prints the following information:
+ * - The size (length) of the list.
+ * - The allocated space for the list.
+ * - The type name of each element in the list.
+ *
+ * @p: The Python list object to inspect.
+ */
 void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t list_size;
-	PyListObject *list; 
+	PyListObject *list;
 	int i;
-	
+
 	list_size = PyList_Size(p);
 	list = (PyListObject *) p;
 
 	printf("[*] Size of the Python List = %li\n", list_size);
 	printf("[*] Allocated = %li\n", list->allocated);
 	for (i = 0; i < list_size; i++)
-	{
-			printf("Element %d: %s\n",
-					i,
-					PyList_GET_ITEM(p, i)->ob_type->tp_name);
-	}
+		printf("Element %d: %s\n", i, PyList_GET_ITEM(p, i)->ob_type->tp_name);
 }
