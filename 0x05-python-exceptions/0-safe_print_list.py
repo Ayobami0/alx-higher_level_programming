b389971b817bc2b996dd26bd9d18e9f29d574672
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-09-25 20:21:13 +0100

    Error fixes 2

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,8 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            if type(my_list[i]) is int:
+                print(my_list[i], end="")
         except IndexError:
             pass
     print("")
