07c139f9bd9f629c75fa97025197829cabfe9f57
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-09-25 23:15:57 +0100

    Error fixes 10

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+    except Exception as e:
+        __import__("sys").stderr.write("Exception: {}\n".format(e))
+        return None
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,4 +6,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        return result
+        print("Inside result: {:f}".format(result))
