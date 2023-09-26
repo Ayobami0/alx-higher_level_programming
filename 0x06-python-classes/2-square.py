fa1584cf0072d6c7596facd659ffd8897240736e
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-09-26 20:38:27 +0100

    Add all files

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,8 +17,8 @@ class Square:
     """
 
     def __init__(self, size=0):
+        self._size = size  #: size of square
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self._size = size  #: size of square
