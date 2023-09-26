e580c7b527574239c69132f66b7976127152ea7c
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-09-26 20:13:30 +0100

    add 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module contains an implementation of the Square class.
+
+    The clase doesnothig for now.
+"""
+
+
+class Square:
+    """The Square class that probably computes the area of a square.
+
+    Instantiates the class with a private field of _size.
+    """
+
+    def __init__(self, size=0):
+        """Initialize the class.
+
+        Sets the class private attr to the parameter size.
+        The size variable must be an integer and  must be greater than 0 else
+        TypeError and ValueError are raised respectively
+
+        Args:
+            size (int): the size of the Square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._size = size
