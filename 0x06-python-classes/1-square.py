57a0e79cadf034339e44c7f37c00be2fc93aac62
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-09-26 20:54:22 +0100

    test fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -13,9 +13,5 @@ class Square:
         size (int): the size of the Square.
     """
 
-    def __init__(self, size=0):
-        self._size = size  #: size of square
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+    def __init__(self, size):
+        self._size = None  #: size of square
