591148896eaaf17de22953bb50678f4c501f449a
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-09-26 23:06:59 +0100

    fixes for 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -100,6 +100,7 @@ be a tuple of 2 positive integers"
             print("")
         else:
             if self.__position[1] != 0:
-                print("" * self.__position[1])
+                for _ in range(self.__position[1]):
+                    print("")
             for _ in range(self.__size):
                 print(" " * self.__position[0], "#" * self.__size, sep="")
