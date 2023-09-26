a1b268304fe669d97184b1a05196a9b0407aa423
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-09-26 20:04:40 +0100

    fixes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""This module contains an implementation of the Square class."""
+"""This module contains an implementation of the Square class.
+
+    The clase doesnothig for now.
+"""
 
 
 class Square:
-    """The Square class.
+    """The Square class that probably computes the area of a square.
 
-    Instantiates the class with a private field of _size
+    Instantiates the class with a private field of _size.
     """
 
     def __init__(self, size):
         """Initialize the class.
 
-        Sets the class private attr to the parameter size
+        Sets the class private attr to the parameter size.
 
         Args:
-            size: the size of the size of the Square
+            size: the size of the Square.
         """
         self._size = size
