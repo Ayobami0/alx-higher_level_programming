963edb6f2e632caf1a04b97d1579a2cfa1ebb78f
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-10-01 23:01:26 +0100

    add 2-matrix_divided

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""This module contains a function that adds 2 integers"""
+"""This module contains a function that adds 2 integers.
+
+The add_integer function will be tested using the similarly named
+text file in the tests folder.
+You can run the test using this command
+    python3 -m doctest -v ./tests/0-add_integer.txt | tail -2
+"""
 
 
 def add_integer(a, b=98):
@@ -8,16 +14,14 @@ def add_integer(a, b=98):
     Args:
         a: an integer or float
         b: an integer or float (has a default value of 98)
-
     Return:
         The result of the addition
-
     Raises:
         TypeError: a and b must both of interger types.
     """
-    if a is not int or a is not float:
+    if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if b is not int or b is not float:
+    if type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""This module contains a function that devides all elements of a matrix.
+
+The matrix_divided function will be tested using the similarly named
+text file in the tests folder.
+You can run the test using this command
+    python3 -m doctest -v ./tests/1-matrix_divided.txt | tail -2
+"""
+
+
+def matrix_divided(matrix, div):
+    """Function to divide elements of a matrix.
+
+    Args:
+        matrix: A list or lists representing a matirx
+        div: number to divide the elements
+    Return:
+        The result of the division in a matrix of the same size
+        as the divided matrix.
+        The values of the matrix should all be rounded up to two
+        decimal places.
+        The matrix will be returned if the matric is empty
+    Raises:
+        TypeError:
+            matrix must be a list of list containing integer or float values
+            div must both of integer type.
+            each row of matrix must be of same size
+        ZeroDivisionError:
+            div cannot be equal to 0
+    """
+    if type(matrix) is not list:
+        raise TypeError(
+            "matrix must be a matrix (list of lists)\
+ of integers/floats"
+        )
+    if type(div) not in [float, int]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if len(matrix) == 0:
+        return matrix
+    len_row = len(matrix[0])
+    for row_idx, row in enumerate(matrix):
+        if type(row) is not list:
+            raise TypeError(
+                "matrix must be a matrix (list of lists)\
+ of integers/floats"
+            )
+        if len(row) != len_row:
+            raise TypeError("Each row of the matrix must have the same size")
+        for val_idx, val in enumerate(row):
+            if type(val) not in [float, int]:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists)\
+ of integers/floats"
+                )
+            row[val_idx] = round(val / div, 2)
+        matrix[row_idx] = row
+    return matrix
