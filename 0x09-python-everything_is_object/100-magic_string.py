54197c509812bd5bf594d96987bb916fedd2c0cf
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-10-04 13:35:27 +0100

    fixes

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def magic_string():
-    print(("BestSchool, " * (globals()[list(globals())[-1]] + 1))[:-2])
+    return ("BestSchool, " * (globals()[list(globals())[-1]] + 1))[:-2]
