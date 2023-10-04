bdab93d43751614ef6fabc36824b0aab62515310
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-10-04 13:45:14 +0100

    Commit fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -19,9 +19,9 @@ def add_integer(a, b=98):
     Raises:
         TypeError: a and b must both of interger types.
     """
-    if type(a) is not int and type(a) is not float:
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if a is None or (type(b) is not int and type(b) is not float):
         raise TypeError("b must be an integer")
     result = a + b
     if result == float("inf") or result == -float("inf"):
