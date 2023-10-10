cab11016390844936fbbb4e7c6d6b65cd26c0650
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-10-10 21:38:39 +0100

    Bug fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,8 @@
-"""A module module containing a function that reads a file"""
+"""A module module containing a function that reads a file."""
 
 
 def read_file(filename=""):
-    """
-    Read and print the contents of a file.
+    """Read and print the contents of a file.
 
     Args:
         filename (str): The name of the file to read
