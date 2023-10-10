75b08b97efa1e2c8b7252ee700529461eacf26b3
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-10-10 21:36:00 +0100

    First commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+"""A module module containing a function that reads a file"""
+
+
+def read_file(filename=""):
+    """
+    Read and print the contents of a file.
+
+    Args:
+        filename (str): The name of the file to read
+        (default is an empty string).
+    Note:
+        If 'filename' is not provided or is an empty string,
+        the function will not read any file.
+    """
+    try:
+        with open(filename, "r", encoding="UTF8") as f:
+            print(f.read())
+    except Exception:
+        pass
