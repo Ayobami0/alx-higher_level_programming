56854139edbd526924bc1fbe60a5f3a998611935
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-12-12 19:50:33 +0100

    feat: add source for 7-8

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  list.forEach(e => {
+    count += e === searchElement ? 1 : 0;
+  });
+  return count;
+};
