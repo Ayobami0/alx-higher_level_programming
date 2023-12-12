6b3c4935974fcf7fd73b2372abe497504646bda6
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-12-12 15:23:01 +0100

    fix: checker fix for Task 6 again

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,38 +1,6 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (!(w <= 0 || h <= 0)) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      let row = '';
-      for (let j = 0; j < this.width; j++) {
-        row += 'X';
-      }
-      console.log(row);
-    }
-  }
-
-  rotate () {
-    const width = this.width;
-    this.width = this.height;
-    this.height = width;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-}
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+const ImportedSquare = require('./5-square');
+class Square extends ImportedSquare {
   charPrint (c) {
     let printSymbol = 'X';
     if (c !== undefined) {
