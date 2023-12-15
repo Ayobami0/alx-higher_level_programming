0389d06fab161982f74bc9be221721fbfd30b98f
Ayobami Oludemi <oludemiayobami@gmail.com>
2023-12-15 15:44:28 +0100

    fix: checker fix for task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""A module for database connectivity"""
 import MySQLdb
 import sys
 
@@ -21,5 +21,6 @@ SELECT * FROM states ORDER BY id
 
 rows = db.store_result().fetch_row(maxrows=0)
 
-for row in rows:
-    print(row)
+if __name__ == "__main__":
+    for row in rows:
+        print(row)
