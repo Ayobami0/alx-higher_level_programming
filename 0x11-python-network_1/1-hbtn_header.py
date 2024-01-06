fec7c65239f3bcf693cfe6854dd4f616f4533290
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-01-06 21:56:06 +0100

    fix(checker): Errors with import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,11 +2,12 @@
 """
 Featches a link and prints the value of the X-Request-Id
 """
-import urllib
-import sys
+if __name__ == '__main__':
+    import urllib.request
+    import sys
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-with urllib.request.urlopen(url) as res:
-    header = res.headers
-    print(header.get("X-Request-Id"))
+    with urllib.request.urlopen(url) as res:
+        header = res.headers
+        print(header.get("X-Request-Id"))
