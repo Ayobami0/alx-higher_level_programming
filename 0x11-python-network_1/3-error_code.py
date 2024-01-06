3911ea467e9e2aa29e0ac96e268881c47a3a2db7
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-01-06 22:00:51 +0100

    fix(checker): Errors with styling 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -3,14 +3,13 @@
 A Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)
 """
-if __name__ == '__main__':
+if __name__ == "__main__":
     import urllib.request
     import urllib.parse
     import urllib.error
     import sys
 
     url = sys.argv[1]
-
 
     try:
         with urllib.request.urlopen(url) as res:
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,12 +3,11 @@
 A Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)
 """
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
     import requests
 
     url = sys.argv[1]
-
 
     res = requests.get(url)
 
