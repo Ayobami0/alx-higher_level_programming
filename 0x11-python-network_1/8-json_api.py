738a7ea9795ba0fa2997ef681ffaef469eaca4b3
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-01-07 20:03:35 +0100

    feat: Commit for task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a letter and sends a
+POST request to http://0.0.0.0:5000/search_user with
+the letter as a parameter.
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    argv = sys.argv
+    if len(argv) < 2:
+        letter = ""
+    else:
+        letter = sys.argv[1]
+
+    res = requests.post("http://0.0.0.0:5000/search_user", data={"q": letter})
+
+    if res.headers.get("Content-Type") == "application/json":
+        jsonBody = res.content
+        try:
+            print("[{}] {}".format(jsonBody["id"], jsonBody["name"]))
+        except KeyError:
+            print("No result")
+    else:
+        print("Not a valid JSON")
