e4b0c655243e63a2963f3be1ddb23d9ab6af5c71
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-01-31 01:19:33 +0100

    feat: Add remaining files

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const request = require('request');
+
+function fetchPeople (page) {
+  if (!fetch) {
+    return;
+  }
+  const url = `https://swapi-api.alx-tools.com/api/people/?page=${page}`;
+  request.get(url, (_, __, body) => {
+    const jBody = JSON.parse(body);
+    jBody.results.filter((people) => {
+      return people.films.includes(
+        `https://swapi-api.alx-tools.com/api/films/${argv[2]}/`
+      );
+    }).forEach(people => {
+      console.log(people.name);
+    });
+    if (jBody.next !== null) {
+      fetchPeople(++page);
+    }
+  });
+}
+
+fetchPeople(1);
