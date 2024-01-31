9adcd9baa5b48a4e8275069e886f20db2cd75f70
Ayobami Oludemi <oludemiayobami@gmail.com>
2024-01-31 01:39:59 +0100

    fix: checker fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,10 +4,10 @@ const { argv } = require('process');
 const request = require('request');
 
 request.get(argv[2], (_, __, body) => {
-  const characterID = 18;
+  const ID = 18;
   console.log(JSON.parse(body).results.filter((film) => {
     return film.characters.includes(
-      `https://swapi-api.alx-tools.com/api/people/${characterID}/`
+      `https://swapi-api.alx-tools.com/api/people/${ID}/`
     );
   }).length);
 });
